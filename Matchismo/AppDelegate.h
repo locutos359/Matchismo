e1564aba1a496d7f76e72bc64e482ad316706184
locutos359 <locutos359@gmail.com>
2013-12-19 14:56:19 -0800

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Matthew Richman on 12/19/13.
+//  Copyright (c) 2013 Matthew Richman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Matthew Richman on 12/19/13.
+//  Copyright (c) 2013 Matthew Richman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
